1e44ca07af93a21808c18788e2f0ab4db0b19724
matej <stavanja@gmail.com>
2017-04-01 13:48:49 +0200

    working example although the card get deleted instead of hidden

--- src/components/Memory.vue
+++ src/components/Memory.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="cont">
-    <card v-bind:active="selected" v-for="el,index in matrix" v-on:clicked="checkMatch" v-bind:element="shuffled" v-bind:index="index" :key="index"></card>
+<div>
+<p>Vpiši, koliko kock želiš (sodo število): <input type="text" v-model="boxes"></input>
+<button @click="startGame()">{{game}}</button>
+<div class="cont">
+
+    <card v-bind:active="selected" v-for="el,index in shuffled" v-on:clicked="checkMatch" v-bind:element="shuffled" v-bind:index="index" :key="index"></card>
+
+
   </div>
+  </div>
+
+
 </template>
 
 <script>
@@ -10,16 +19,20 @@
     name: 'Memory',
     data: function () {
       return {
-        rows: 2,
+        boxes: 2,
         selectedCount: 0,
         shuffled: [],
-        selected: []
+        selected: [],
+        game: 'Začni z igro'
       }
     },
     components: {
       'card': Card
     },
     computed: {
+      rows () {
+        return this.boxes / 2
+      },
       matrix () {
         // http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
         /**
@@ -62,7 +75,13 @@
           if (indexOfSelected !== index) {
             this.selected.splice(index, 1, 1)
             if (this.shuffled[indexOfSelected] === this.shuffled[index]) {
-              console.log('match')
+              let value = this.shuffled[index]
+              setTimeout(() => {
+                this.selected = this.selected.map(x => 0).slice(0, -2)
+                this.shuffled = this.shuffled.filter(x => {
+                  return x !== value
+                })
+              }, 1000)
             }
             this.selectedCount += 1
           } else {
@@ -74,6 +93,35 @@
           this.selected.splice(index, 1, 1)
           this.selectedCount += 1
         }
+      },
+      startGame () {
+        // http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+        /**
+         * Randomize array element order in-place.
+         * Using Durstenfeld shuffle algorithm.
+         */
+        function shuffleArray (array) {
+          for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1))
+            var temp = array[i]
+            array[i] = array[j]
+            array[j] = temp
+          }
+          return array
+        }
+        const upperLimit = this.rows * this.rows
+        let matrix = []
+        for (let i = 1; i < upperLimit + 1; i++) {
+          matrix.push(i)
+        }
+        // select only the first half of the elements in a shuffled array
+        let shuffled = shuffleArray(matrix).slice(0, upperLimit / 2)
+        // just concat the shuffled array with itself to get two of each element
+        const newArr = shuffled.concat(shuffled)
+        this.shuffled = newArr
+        this.selected = newArr.map(x => {
+          return 0
+        })
       }
     }
   }
@@ -84,7 +132,8 @@
   .cont {
     display: flex;
     margin: 0 auto;
-    width: 800px;
+    width: 80%;
     flex-wrap: wrap;
+
   }
 </style>
